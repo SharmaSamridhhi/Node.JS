d3ff1dafe6a5b517ab1a321b1f1d818434609bb7
Samridhhi Sharma <sharma.samridhhi9@gmail.com>
2024-06-25 16:09:21 +0530

    changes

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ app.get("/about", (req, res) => {
   return res.send("Welcome to about page!!");
 });
 app.get("/contact", (req, res) => {
-  return res.send(
-    "Welcome " + req.query.name + "! your number is +91-111 111 1111"
-  );
+  return res.send(`Welcome ${req.query.name}! your number is +91-111 111 1111`);
 });
 
 app.listen(8000, () => console.log("Server Started!!!!"));
