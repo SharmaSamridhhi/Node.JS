45f58287b1847fc18c5f445190163344920c1abd
Samridhhi Sharma <sharma.samridhhi9@gmail.com>
2024-06-25 16:03:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("Welcome back!!");
+});
+app.get("/about", (req, res) => {
+  return res.send("Welcome to about page!!");
+});
+app.get("/contact", (req, res) => {
+  return res.send(
+    "Welcome " + req.query.name + "! your number is +91-111 111 1111"
+  );
+});
+
+app.listen(8000, () => console.log("Server Started!!!!"));
